6a48a01ef5fb868602040613de7481064b9a360a
VDToader <VDToader@gmail.com>
2024-05-06 16:56:48 +0100

    Nothing
    .

--- FitnessTracker/src/main/java/com/mycompany/fitnesstracker/FitnessTracker.java
+++ FitnessTracker/src/main/java/com/mycompany/fitnesstracker/FitnessTracker.java
@@ -8,6 +8,8 @@ package com.mycompany.fitnesstracker;
  *
  * @author BlackBox
  */
+import java.util.ArrayList;
+import java.util.List;
 public class FitnessTracker {
 
     public static void main(String[] args) {
@@ -44,7 +46,31 @@ public class FitnessTracker {
             switch (choice) {
                     case 1:
                         System.out.println("You've chosen View available exercises");
-
+                        List<Exercise> exercises = new ArrayList<>();
+                        // Create 10 new objects of the Class Exercise and display the array list containing them
+                        exercises.add(new Exercise(1,"Lunges", 30, 10, "Like squats, lunges work all the major muscles of the lower body: gluteals, quadriceps, and hamstrings"));
+                        exercises.add(new Exercise(2,"Plank", 45, 15, "Forearm plank. A full-body exercise that requires strength and balance, planks put the core into overdrive."));
+                        exercises.add(new Exercise(3,"Squats", 30, 20, "Squats increase lower body and core strength, as well as flexibility in your lower back and hips."));
+                        exercises.add(new Exercise(4,"Dumbbell rows", 5, 2, "Dumbbell rows. Not only will these make your back look killer in that dress, but dumbbell rows are also another compound exercise that strengthens multiple muscle groups"));
+                        exercises.add(new Exercise(5,"Pushup", 5, 2, "Adding a pushup to your pike will target those shoulders even more. "));
+                        exercises.add(new Exercise(6,"Crunches", 10, 4, "Crunches work the ab muscles; [they're] not to be mistaken as exercise that burns the fat over the abdominals."));
+                        exercises.add(new Exercise(7,"Deadlift", 10, 3, " Deadlift. The bread and butter of countless gym routines, this move, if done properly, will predominantly engage your back and legs."));
+                        exercises.add(new Exercise(8,"Lateral raise", 10, 2, "This move is similar to regular lateral raises, except here you raise the weights while slightly bent over, leading to more rear deltoid and trap development."));
+                        exercises.add(new Exercise(9,"Bridge", 10, 2, " Activate your core and posterior chain (a fancy term for the backside of your body) with a bridge."));
+                        exercises.add(new Exercise(10,"Burpee", 5, 2, "The burpee, a squat thrust with an additional stand between repetitions, is a full body exercise used in strength training."));
+                        // Display all existing exercises
+                        Exercise.displayAllExercises(exercises);
+                        
+                        //  Ask the user if he wants to add an exercise or more to the favourite list.
+                        System.out.println("Would you like to add an exercise to your favourite list? (1 for YES or 0 for NO) ");
+                        mainMenu.getUserChoice();
+                        if (choice == 1){
+                            System.out.println("Which exercise would you like to add? (Enter the option number)");
+                            // Add the chosen element with the specific ID to the favourite list
+                            System.out.println("This exercise will be added to the favourite list: ");
+                            mainMenu.getUserChoice();
+                        }
+                        else
                         break;
 
                     case 2:
