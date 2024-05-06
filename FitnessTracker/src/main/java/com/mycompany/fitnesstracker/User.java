b2f904299d1899ac1a6408035e88fd6079aaa61d
VDToader <VDToader@gmail.com>
2024-05-06 13:11:14 +0100

    Modified instance variables
    Removed email attribute and added a data field named password. Also disabled the setter for admin because is no use at this stage.

--- FitnessTracker/src/main/java/com/mycompany/fitnesstracker/User.java
+++ FitnessTracker/src/main/java/com/mycompany/fitnesstracker/User.java
@@ -9,13 +9,15 @@ package com.mycompany.fitnesstracker;
  */
 public class User {
     private String name;
-    private String email;
-    private boolean isAdmin;
+    private String password;
+    private int userId;
+    private boolean isAdmin = false;
 
     // USER CLASS CONSTRUCTOR
-    public User (String name, String email, boolean isAdmin){
+    public User (String name, String password, int userId, boolean isAdmin){
         this.name = name;
-        this.email = email;
+        this.password = password;
+        this.userId = userId;
         this.isAdmin = false;
     }
     
@@ -23,24 +25,27 @@ public class User {
     public String getName(){
         return name;   
     }
-    public String getEmail(){
-        return email;
+    public String getPassword(){
+        return password;
+    }
+    public int getUserId(){
+        return userId;
     }
     public boolean getIsAdmin(){
         return isAdmin;
     }
     
-    // SETTERS
+    // SETTERS ( no need right now for the admin)
     public void setName(String name){
         this.name = name;
     }
-    public void setEmail(String email){
-        this.email = email;
+    public void setPassword(String password){
+        this.password = password;
     }
-    public void setIsAdmin(boolean isAdmin){
-        this.isAdmin = isAdmin;
+    public void setUserId (int userId){
+        this.userId = userId;
     }
-    
+
 
 }
     
