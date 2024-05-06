5f7fa39982604f5590732fb8988699c9fb59ba15
VDToader <VDToader@gmail.com>
2024-05-06 16:56:33 +0100

    Id attribute
    Added the id attribute to help assigning the chosen option to a defined id.

--- FitnessTracker/src/main/java/com/mycompany/fitnesstracker/Exercise.java
+++ FitnessTracker/src/main/java/com/mycompany/fitnesstracker/Exercise.java
@@ -8,19 +8,27 @@ package com.mycompany.fitnesstracker;
  *
  * @author BlackBox
  */
+import java.util.List;
+import java.util.ArrayList;
+
 public class Exercise {
+    private int id;
     private String name;
     private int duration;
     private int burnedKcal;
     private String description;
    
-    public Exercise (String name, int duration, int burnedKcal, String description){
+    public Exercise (int id, String name, int duration, int burnedKcal, String description){
+        this.id = id;
         this.name = name;
         this.duration = duration;
         this.burnedKcal = burnedKcal;
         this.description = description;
     }
     // Declaring Accessors(Getters) for the Exercise Class
+    public int getId(){
+        return id;
+    }
     public String getName (){
         return name;
     }
@@ -30,8 +38,14 @@ public class Exercise {
     public int getBurnedKcal () {
         return burnedKcal;
     }
-    public String description () {
+    public String getDescription(){
         return description;
     }
-    
+    public static void displayAllExercises(List<Exercise> exercises) {
+        System.out.println("Existing Exercises:");
+        for (Exercise exercise : exercises) {
+            System.out.println("Option "+exercise.getId()+ "------ Name: " +exercise.getName() + "| Duration: " + exercise.getDuration() + " seconds" + "| Burned Kcal: " + exercise.getBurnedKcal() + " Kcal " + " | Description: " + exercise.getDescription());
+        }
+ 
+    }
 }
